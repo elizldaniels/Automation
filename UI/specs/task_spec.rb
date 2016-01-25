d77263c3ef33c3a9c734690c96740097e2bbd76a
elizldaniels <elizldaniels@gmail.com>
2016-01-25 08:34:03 -0800

    pushing updates

--- UI/specs/task_spec.rb
+++ UI/specs/task_spec.rb
@@ -1,17 +1,18 @@
 require 'selenium-webdriver'
-require_relative '../pages/template'
+require_relative '../pages/task'
 require_relative '../pages/login'
 require_relative 'spec_helper'
 
-describe 'Template' do
+describe 'Task' do 
 
 	before(:each) do
 		@login = Login.new(@driver)
-		@template = Template.new(@driver)
+		@task = Task.new(@driver)
 	end
 
 	it 'will create' do
 		@login.with('liz@toutapp.com', 'Monkeyw1!')
-		@template.create()
+		@task.create()
 	end
+	
 end
